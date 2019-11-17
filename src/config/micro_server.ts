c39429726fb33fbb8f71cbbddd075be864223c8b
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-11-18 01:20:23 +0700

    up

--- src/config/micro_server.ts
+++ src/config/micro_server.ts
@@ -3,7 +3,7 @@ const n = process.env.NODE_ENV
 export const server_user =
   n === 'development' || n === 'test'
     ? 'http://localhost:3001'
-    : process.env.SERVER_USER || 'DOMAIN_NAME_HERE'
+    : process.env.SERVER_USER || 'https://mole-server-user.crowfx.online/'
 
 export const server_promo =
   n === 'development' || n === 'test'
